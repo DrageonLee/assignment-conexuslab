40ff0d7a8d13a60b8f1aa6e51561c5538465d836
drageon lee <yg910524@gmail.com>
2022-04-18 02:32:24 +0900

    feat(get_numbers) : add input numbers function

--- baseball_game.py
+++ baseball_game.py
@@ -11,4 +11,15 @@ def generate_numbers():
         
     return numbers
 
-print(generate_numbers())
+def get_numbers():
+    number = input("숫자를 입력해 주세요 : ")
+    num_list = []
+    if len(set(number)) != 3 or len(number) !=3:
+        print("세가지 숫자를 각각 다르게 다시 입력해 주세요! ")
+        return get_numbers()
+    for i in range(3):
+        num_list.append(int(number[i]))
+    
+    return num_list
+
+print(get_numbers())
